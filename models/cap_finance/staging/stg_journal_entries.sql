0b33bd106195e5ffe92d19f694361201f349e4b8
akarsh <akarsh.asok@gmail.com>
2026-02-25 21:46:57 -0500

    Minor sqlfluff fix

--- models/cap_finance/staging/stg_journal_entries.sql
+++ models/cap_finance/staging/stg_journal_entries.sql
@@ -1,6 +1,6 @@
 {{ config(
     materialized='table',
-    cluster_by=['posting_date', 'entity', 'account'],
+    cluster_by=['posting_date', 'legal_entity', 'account'],
     partition_by={'field': 'posting_date', 'data_type': 'date'}
 ) }}
 
@@ -8,24 +8,24 @@ SELECT
   {{ dbt_utils.generate_surrogate_key([
     'je_id',
     'posting_date',
-    'account',
-    'entity',
-    'debit',
-    'credit',
+    '`account`',
+    '`entity`',
+    '`debit`',
+    '`credit`',
     'memo'
   ]) }} AS je_line_sk,
-  je_id,
-  posting_date,
-  UPPER(account) AS account,
-  UPPER(entity) AS entity,
-  debit,
-  credit,
-  credit - debit AS gl_net_movement,
-  memo,
-  ingested_at
-FROM {{ source('raw', 'journal_entries') }}
+  p.je_id,
+  p.posting_date,
+  UPPER(p.`account`) AS `account`,
+  UPPER(p.`entity`) AS legal_entity,
+  p.`debit` AS debit,
+  p.`credit` AS credit,
+  p.`credit` - p.`debit` AS gl_net_movement,
+  p.memo,
+  p.ingested_at
+FROM {{ source('raw', 'journal_entries') }} AS p
 QUALIFY
   ROW_NUMBER() OVER (
     PARTITION BY je_line_sk
-    ORDER BY ingested_at DESC
+    ORDER BY p.ingested_at DESC
   ) = 1
